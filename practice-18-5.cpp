eb40f8dbe7d3dbc13df3bac602471fa2c92b08c5
kirillkychkin <kychkin.kirill@gmail.com>
2022-02-23 11:06:43 +0900

    initial commit

---
+++ practice-18-5.cpp
@@ -0,0 +1,16 @@
+// practice-18-5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Stack
+{
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
